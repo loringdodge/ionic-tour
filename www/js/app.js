1e3d94237a0d56885c83d928be507f408a96f53c
Loring Dodge <loringdodge@yahoo.com>
2015-09-12 10:38:07 -0700

    Update demo code

--- www/js/app.js
+++ www/js/app.js
@@ -27,22 +27,18 @@ angular.module('starter', ['ionic', 'ionic.tour'])
   $ionicTour.fromTemplateUrl('tour-template.html', {
     scope: $scope
   }).then(function(tour){
-    console.log('tourLoaded');
     $scope.tour = tour;
   });
 
   $scope.next = function() {
-    // console.log('next');
     $scope.tour.next();
   }
 
   $scope.previous = function() {
-    // console.log('previous');
     $scope.tour.previous();
   }
 
   $scope.finish = function() {
-    // console.log('finish');
     $scope.tour.finish({
       destroy: false
     });
@@ -53,44 +49,45 @@ angular.module('starter', ['ionic', 'ionic.tour'])
   }
 
   $scope.reset = function() {
-    // console.log('reset');
     $scope.tour.reset();
   }
 
   $scope.onStart = function(element, tourtip) {
-    // console.log('onStart', element, tourtip);
     angular.element(element).addClass('button-outline button-assertive')
   }
 
   $scope.onEnd = function(element, tourtip) {
-    // console.log('onEnd', element, tourtip);
+
   }
 
   $scope.onLeave = function(element, tourtip) {
-    // console.log('onLeave', element, tourtip);
     angular.element(element).removeClass('button-outline button-assertive')
   }
 
   $scope.onTransition = function(ratio) {
-    // console.log('onTransition', ratio);
+
   }
 
-  $scope.$on('tourFinished', function(){
-    // console.log('tourFinished');
+  $scope.$on('tourAtStart', function(){
+
   })
 
-  $scope.$on('tourStarted', function(){
-    // console.log('tourStarted');
+  $scope.$on('tourAtEnd', function(){
+
   })
 
   $scope.openSideMenu = function(element, tourtip) {
     $ionicSideMenuDelegate.toggleLeft(true);
   }
 
-  $scope.closeSideMenu = function(element, tourtip) {
+  $scope.closeSideMenuPrevious = function(element, tourtip) {
     if($scope.tour._orientation === 'previous') {
       $ionicSideMenuDelegate.toggleLeft(false);
     }
+  }
+
+  $scope.closeSideMenu = function(element, tourtip) {
+    $ionicSideMenuDelegate.toggleLeft(false);
   }
 
 
